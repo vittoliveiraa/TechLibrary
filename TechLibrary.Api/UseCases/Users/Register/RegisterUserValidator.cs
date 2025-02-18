3803fb9185359c343f6ec7d4128a6880063085be
Victor Oliveira <vittoliveiraa@live.com>
2025-02-18 17:04:39 -0300

    Criação da solução e da primeira parte da Aula 1

---
+++ TechLibrary.Api/UseCases/Users/Register/RegisterUserValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using TechLibrary.Communication.Requests;
+
+namespace TechLibrary.Api.UseCases.Users.Register
+{
+    public class RegisterUserValidator : AbstractValidator<RequestUserJson>
+    {
+        public RegisterUserValidator()
+        {
+            RuleFor(request => request.Name).NotEmpty().WithMessage("Nome não pode ser vazio");
+            RuleFor(request => request.Email).EmailAddress().WithMessage("O email não válido");
+            RuleFor(request => request.Password).NotEmpty().WithMessage("A senha é obrigatória");
+            When(request => string.IsNullOrEmpty(request.Password) == false, () =>
+            {
+                RuleFor(request => request.Password.Length).GreaterThanOrEqualTo(6).WithMessage("A senha deve conter mais que 6 caracteres");
+            });
+        }
+    }
+}
---
+++ TechLibrary.Communication/Responses/ResponseErrorMessagesJson.cs
@@ -0,0 +1,8 @@
+﻿namespace TechLibrary.Communication.Responses
+{
+    public class ResponseErrorMessagesJson
+    {
+        public List<string> Errors { get; set; } = [];
+
+    }
+}
